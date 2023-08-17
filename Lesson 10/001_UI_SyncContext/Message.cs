ef6eb889d9fff62ae3ef617534a2322ab51bac5f
Radmir <radmirt@mail.ru>
2023-08-17 16:12:19 +0500

    Добавлен урок 10

---
+++ Lesson 10/001_UI_SyncContext/Message.cs
@@ -0,0 +1,12 @@
+﻿internal class Message
+{
+    public SendOrPostCallback Callback { get; }
+    public object State { get; }
+
+    public Message(SendOrPostCallback callback, object state)
+    {
+        Callback = callback;
+        State = state;
+    }
+}
+
